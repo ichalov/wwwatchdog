b9a91fa2d3414d80eda19f8394430eee925743de
Victor Ichalov <ichalov@gmail.com>
2015-06-14 14:11:06 +0300

    Fix page length test functioning to use the new user agent injection.

--- test-suite.pl
+++ test-suite.pl
@@ -35,7 +35,6 @@ my $get_ua = sub {
 };
 
 $msg = ""; wwwatchdog::process($targets, $ntf, $get_ua);
-
 ok($msg eq '', "Base case scenario.");
 
 ### 
@@ -50,14 +49,31 @@ my $targets_length = {
   }
 };
 
-$msg = ""; wwwatchdog::process($targets_length, $ntf, $get_ua);
+my $get_ua_length;
+{
+  my $cnt;
+  $get_ua_length = sub {
+    use Test::LWP::UserAgent;
 
+    my $ua = new Test::LWP::UserAgent;
+    if (++$cnt == 1) {
+      $ua->map_response('www.google.com', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], ''));
+    }
+    else {
+      $ua->map_response('www.google.com', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], 'X'x2048));
+    }
+
+    return $ua;
+  };
+}
+
+$msg = ""; wwwatchdog::process($targets_length, $ntf, $get_ua_length);
 like($msg, qr{length is less than expected}, "Length threshold. Negative part.");
+
+$msg = ""; wwwatchdog::process($targets_length, $ntf, $get_ua_length);
+like($msg, qr{restored functioning}, "Length threshold. Positive part.");
 
 ###
 
-$msg = ""; wwwatchdog::process($targets, $ntf, $get_ua);
-
-like($msg, qr{restored functioning}, "Length threshold. Positive part.");
 
 done_testing;
