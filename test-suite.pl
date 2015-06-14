6cec514e352b4c0ca2d8c4550e81682ad7bad65f
Victor Ichalov <ichalov@gmail.com>
2015-06-14 14:35:40 +0300

    Add page status test.

--- test-suite.pl
+++ test-suite.pl
@@ -3,6 +3,9 @@
 use strict;
 use warnings;
 
+`rm -f ./error_flag_*`;
+`rm -f ./slow_flag_*`;
+
 use Test::More;
 
 use_ok('wwwatchdog');
@@ -17,7 +20,7 @@ my $ntf = sub {
 ###
 
 my $targets = {
-  'http://www.google.com' => {
+  'http://www.test' => {
     'uris' => {
       '/' => {
       }
@@ -29,7 +32,7 @@ my $get_ua = sub {
   use Test::LWP::UserAgent;
   my $ua = new Test::LWP::UserAgent;
 
-  $ua->map_response('www.google.com', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], ''));
+  $ua->map_response('www.test', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], ''));
 
   return $ua;
 };
@@ -40,7 +43,7 @@ ok($msg eq '', "Base case scenario.");
 ### 
 
 my $targets_length = {
-  'http://www.google.com' => {
+  'http://www.test' => {
     'uris' => {
       '/' => {
         'length_threshold' => 1024,
@@ -57,10 +60,10 @@ my $get_ua_length;
 
     my $ua = new Test::LWP::UserAgent;
     if (++$cnt == 1) {
-      $ua->map_response('www.google.com', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], ''));
+      $ua->map_response('www.test', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], ''));
     }
     else {
-      $ua->map_response('www.google.com', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], 'X'x2048));
+      $ua->map_response('www.test', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], 'X'x2048));
     }
 
     return $ua;
@@ -75,5 +78,41 @@ like($msg, qr{restored functioning}, "Length threshold. Positive part.");
 
 ###
 
+my $targets_status = {
+  'http://www.test' => {
+    'uris' => {
+      '/' => {
+        'target_status' => 404,
+      }
+    }
+  }
+};
+
+my $get_ua_status;
+{
+  my $cnt;
+  $get_ua_status = sub {
+    use Test::LWP::UserAgent;
+
+    my $ua = new Test::LWP::UserAgent;
+    if (++$cnt == 1) {
+      $ua->map_response('www.test', HTTP::Response->new('200', 'OK', ['Content-Type' => 'text/plain'], ''));
+    }
+    else {
+      $ua->map_response('www.test', HTTP::Response->new('404', 'Not Found', ['Content-Type' => 'text/plain'], ''));
+    }
+
+    return $ua;
+  };
+}
+
+$msg = ""; wwwatchdog::process($targets_status, $ntf, $get_ua_status);
+like($msg, qr{status differs from expected}, "Page status check. Negative part.");
+
+$msg = ""; wwwatchdog::process($targets_status, $ntf, $get_ua_status);
+like($msg, qr{restored functioning}, "Page status check. Positive part.");
+
+
+###
 
 done_testing;
